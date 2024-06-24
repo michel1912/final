5216bb8c498b1a1953b25d6b9b0f74be751c5fce
michel1912 <michel1912@github.com>
2024-06-24 13:50:29 +0300

    update the integration tests

--- integration_tests/GenerateRosMiddlewareTests.cs
+++ integration_tests/GenerateRosMiddlewareTests.cs
@@ -11,8 +11,12 @@ namespace integration_tests
         public void TranslateAM_ValidContent_ReturnsCorrectJson()
         {
             // Arrange
-            string amContent = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/AMContent.txt");
-            string expectedJson = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/AMJson.json");
+            string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../../"));
+            string amContentPath = Path.Combine(projectRoot, "integration_tests", "TestData", "AMContent.txt");
+            string expectedJsonPath = Path.Combine(projectRoot, "integration_tests", "TestData", "AMJson.json");
+
+            string amContent = File.ReadAllText(amContentPath);
+            string expectedJson = File.ReadAllText(expectedJsonPath);
             string skillName = "navigate";
 
             // Act
@@ -22,13 +26,19 @@ namespace integration_tests
             Assert.Equal(NormalizeJsonString(expectedJson), NormalizeJsonString(jsonResult));
         }
 
+
+
         ////////////////////////////////// INTEGRATION TEST OF TRANSLATE SD FILE ///////////////////////////////////////
         [Fact]
         public void TranslateSD_ValidContent_ReturnsCorrectJson()
         {
             // Arrange
-            string sdContent = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/sdContent.txt");
-            string expectedJson = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/expectedJson.json");
+            string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../../"));
+            string sdContentPath = Path.Combine(projectRoot, "integration_tests", "TestData", "sdContent.txt");
+            string expectedJsonPath = Path.Combine(projectRoot, "integration_tests", "TestData", "expectedJson.json");
+
+            string sdContent = File.ReadAllText(sdContentPath);
+            string expectedJson = File.ReadAllText(expectedJsonPath);
             string skillName = "navigate";
 
             // Act
@@ -38,13 +48,17 @@ namespace integration_tests
             Assert.Equal(NormalizeJsonString(expectedJson), NormalizeJsonString(jsonResult));
         }
 
-        ///////////////////////////////////// INTEGRATION TEST OF TRANSLATE EF FILE ////////////////////////////////////
+        ///////////////////////////////////// INTEGRATION TEST OF TRANSLATE EF FILE //////////////////////////////////////
         [Fact]
         public void TranslateEnvironment_ValidContent_ReturnsCorrectJson()
         {
             // Arrange
-            string environmentContent = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/EFContent.txt");
-            string expectedJson = File.ReadAllText("/home/michel/AOS/check/integration_tests/TestData/EFJson.json");
+            string projectRoot = Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, @"../../../../"));
+            string environmentContentPath = Path.Combine(projectRoot, "integration_tests", "TestData", "EFContent.txt");
+            string expectedJsonPath = Path.Combine(projectRoot, "integration_tests", "TestData", "EFJson.json");
+
+            string environmentContent = File.ReadAllText(environmentContentPath);
+            string expectedJson = File.ReadAllText(expectedJsonPath);
             string skillName = "navigate";
 
             // Act
@@ -53,6 +67,7 @@ namespace integration_tests
             // Assert
             Assert.Equal(NormalizeJsonString(expectedJson), NormalizeJsonString(jsonResult));
         }
+
 
         private string NormalizeJsonString(string json)
         {
