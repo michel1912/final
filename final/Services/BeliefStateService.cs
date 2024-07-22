9c752743f9c7bee93d1f612ccc9dc6e702d5d560
nataliea772 <74179604+nataliea772@users.noreply.github.com>
2024-07-22 21:31:32 +0300

    Update BeliefStateService.cs

--- final/Services/BeliefStateService.cs
+++ final/Services/BeliefStateService.cs
@@ -1,6 +1,6 @@
-using System; 
+using System;
 using MongoDB.Driver;
-using MongoDB.Bson;  
+using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
 using MongoDB.Bson.Serialization.IdGenerators;
 using WebApiCSharp.Models;
@@ -12,33 +12,29 @@ namespace WebApiCSharp.Services
     {
         public static IMongoCollection<BsonDocument> BeliefStateCollection = dbAOS.GetCollection<BsonDocument>(Globals.BELIEF_STATES_COLLECTION_NAME);
 
-public static int GetNumOfStatesSavedInCurrentBelief()
-{
+        public static int GetNumOfStatesSavedInCurrentBelief()
+        {
             var count = BeliefStateCollection.Aggregate(new AggregateOptions())
-            .Match(Builders<BsonDocument>.Filter.Eq("ActionSequnceId", -1))
-            .Project(new BsonDocument("count", new BsonDocument("$size", "$BeliefeState"))).FirstOrDefault();
+                .Match(Builders<BsonDocument>.Filter.Eq("ActionSequnceId", -1))
+                .Project(new BsonDocument("count", new BsonDocument("$size", "$BeliefeState"))).FirstOrDefault();
 
-            if(count == null)return 0;
+            if (count == null) return 0;
             else
             {
                 return count["count"].AsInt32;
             }
-            /*
-                        var filter = Builders<BsonDocument>.Filter.Eq("ActionSequnceId",-1);
-                        var result = BeliefStateCollection.Find(filter).ToList();
-                        if(result.Count == 0)return 0;
-                        return 0;*/
         }
 
         public static List<BsonDocument> GetBeliefForExecution(int skip, int take, int? actionSequnceId = null)
         {
             try
             {
-                //var filter = Builders<BsonDocument>.Filter.Eq("ActionSequnceId" , 1);
-                var filter = actionSequnceId == null ? Builders<BsonDocument>.Filter.Empty : Builders<BsonDocument>.Filter.Eq("ActionSequnceId" , actionSequnceId.Value);
+                var filter = actionSequnceId == null
+                    ? Builders<BsonDocument>.Filter.Empty
+                    : Builders<BsonDocument>.Filter.Eq("ActionSequnceId", actionSequnceId.Value);
                 var project = Builders<BsonDocument>.Projection.Slice("BeliefeState", skip, take);
                 var result = BeliefStateCollection.Find(filter).Project(project).ToList();
-                return result; 
+                return result;
             }
             catch (Exception ex)
             {
@@ -51,11 +47,12 @@ public static int GetNumOfStatesSavedInCurrentBelief()
         {
             try
             {
-                var filter = actionSequnceId == null ? Builders<BsonDocument>.Filter.Empty : Builders<BsonDocument>.Filter.Eq("ActionSequnceId" , actionSequnceId.Value);
-                //var filter = Builders<BsonDocument>.Filter.Eq("ActionSequnceId" , 1);
+                var filter = actionSequnceId == null
+                    ? Builders<BsonDocument>.Filter.Empty
+                    : Builders<BsonDocument>.Filter.Eq("ActionSequnceId", actionSequnceId.Value);
                 var project = Builders<BsonDocument>.Projection.Slice("BeliefeState", skip, take);
                 var result = BeliefStateCollection.Find(filter).Project(project).ToList();
-                return result; 
+                return result;
             }
             catch (Exception ex)
             {
@@ -63,6 +60,5 @@ public static int GetNumOfStatesSavedInCurrentBelief()
                 return null;
             }
         }
-  
     }
 }
