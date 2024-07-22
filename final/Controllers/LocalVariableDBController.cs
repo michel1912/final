a4e656c923a46e479b64badf120f6d9e2f8af834
nataliea772 <74179604+nataliea772@users.noreply.github.com>
2024-07-22 21:10:26 +0300

    Update LocalVariableDBController.cs

--- final/Controllers/LocalVariableDBController.cs
+++ final/Controllers/LocalVariableDBController.cs
@@ -1,16 +1,15 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MongoDB.Bson;  
+using MongoDB.Bson;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using WebApiCSharp.Models;
 using WebApiCSharp.Services;
+
 namespace WebApiCSharp.Controllers;
 
-
-//add for new feature
 [ApiController]
 [Route("[controller]")]
 public class LocalVariableDBController : ControllerBase
@@ -21,45 +20,41 @@ public class LocalVariableDBController : ControllerBase
     {
         _logger = logger;
     }
+
     [HttpGet]
     public List<LocalVariableDB> GetAll()
     {
         return LocalVariableDBService.Get();
     }
-    
+
     [HttpGet("{id}")]
     public ActionResult<LocalVariableDB> Get(int id)
     {
         var localVar = LocalVariableDBService.Get(id);
-        if(localVar == null)
+        if (localVar == null)
             return NotFound();
         return localVar;
     }
-    
+
     [HttpPost]
     public IActionResult Create(LocalVariableDB localVar)
     {
         LocalVariableDB n = LocalVariableDBService.Add(localVar);
-        if(n == null)
+        if (n == null)
             return Conflict();
         return CreatedAtAction(nameof(Create), n);
     }
+
     [HttpPut("{id}")]
     public IActionResult Update(LocalVariableDB localVar)
     {
         LocalVariableDBService.Update(localVar);
         return NoContent();
     }
+
     [HttpDelete("{id}")]
     public IActionResult Delete(string id)
     {
-        // var lVar = LocalVariableService.Get(id);
-
-        // if (lVar is null)
-        //     return NotFound();
-
-        // LocalVariableDBService.Delete(new LocalVariable() { Id = BsonObjectId(id) });
-
         return NoContent();
     }
 }
