480aecf39d3696736ec25c893d64583e41c55c23
nataliea772 <74179604+nataliea772@users.noreply.github.com>
2024-07-22 21:11:47 +0300

    Update LogMessageController.cs

--- final/Controllers/LogMessageController.cs
+++ final/Controllers/LogMessageController.cs
@@ -1,7 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using MongoDB.Bson;  
+using MongoDB.Bson;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -13,22 +13,20 @@ namespace WebApiCSharp.Controllers
     [ApiController]
     [Route("[controller]")]
     public class LogMessageController : ControllerBase
-    { 
-
+    {
         private readonly ILogger<LogMessageController> _logger;
 
         public LogMessageController(ILogger<LogMessageController> logger)
         {
             _logger = logger;
         }
-
-
+        
         [HttpGet]
         public List<LogMessage> GetAll()
         {
-            return LogMessageService .Get();
+            return LogMessageService.Get();
         }
-      
+
         [HttpDelete]
         public IActionResult Delete(int id)
         {
